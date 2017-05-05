58930e755a268de3b57e6312f006e88e95f09368
demo <demo@mailinator.com>
2017-05-05 13:13:57 +0530

    add delete

--- biddingSystem/admin/view/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-include('../model/functions.php');
-
-header("location:index.php");
-session_unset($_SESSION['id']);
-?>
--- session/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-session_unset($_SESSSION["myname"]);
-header("location:login.php");
-
-?>
