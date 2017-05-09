7d6a913d3f2c094502a526d16711904fe2cffbd3
username <demo@mailinator.com>
2017-05-09 14:31:33 +0530

    sloved

--- login.php
+++ login.php
@@ -46,7 +46,15 @@ if($_SESSION['userid'])
 
 
 <?php
+<<<<<<< HEAD
 echo "demo";
+=======
+<<<<<<< HEAD
+echo "test";
+=======
+echo "demo";
+>>>>>>> master
+>>>>>>> abc
 include('connection.php');
 if(isset($_REQUEST["login"]))
 {
